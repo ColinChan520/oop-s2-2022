afb8e09af652ff6bca9efdca8ae318fd3657f253
ColinChan520 <a1864874@adelaide.edu.au>
2022-08-05 19:51:09 +0930

    1

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 using namespace std;
+void print_summed(int array1[3][3],int array2[3][3]){
+int sum[3][3] = {0}; 
 
-void print_summed(int array1[3][3],int array2[3][3])
-{
-    for(int i=0;i<3;i++) //loop to rows 
-    {
+    for(int i=0;i<3;i++)  {
         for(int j=0;j<3;j++) //loop to column
         {
-            cout<<array1[i][j]+array2[i][j]<<" "; //add together 
+           sum[i][j] = array1[i][j]+array2[i][j]; //add together 
         }
-        cout<<endl; //newline 
     }
-    
-    
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j <3 ; j++)
+        {
+            if (j != 0) {
+                cout<< " ";
+            }
+            cout<< sum[i][j];
+        }
+        cout<< "\n";
+    }    
 }
+
 
